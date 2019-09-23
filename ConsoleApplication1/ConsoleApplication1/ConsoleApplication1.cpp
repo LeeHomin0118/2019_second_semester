2f4a03d2b983288fad52eafafb58916844aac855
hominlaptop <leehomin118@gmail.com>
2019-09-23 21:23:59 +0900

    add python

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,60 +1,20 @@
 ﻿#include <stdio.h>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 using namespace std;
+typedef pair<int, int> arr;
+int arr[1000000];
 int main() {
-	int n;
-	const int num = 100;
-	vector <pair<int, int>> arr[num];
-	scanf("%d", &n);
+	int n, m, c1, c2;
+	scanf("%d %d %d %d", &n, &m, &c1, &c2);
 	for (int i = 0; i < n; i++) {
-		scanf("%d", &arr[i].first);
-		arr[i].second = i;
+		int tmp;
+		scanf("%d", &tmp);
+		arr[i] = { tmp, 0 };
 	}
-	sort(arr, arr + n);
-	printf("%d %d", arr[n - 1].first, arr[n - 1].second);
+	for (int i = 0; i < m; i++) {
+		int tmp;
+		scanf("%d", &tmp);
+		arr[n + i] = { tmp, 1 };
+	}
 }
-
-
-
-
-
-
-/*
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <string>
-#include <iostream>
-using namespace std;
-int main() {
-	char a, b;
-	string str1, str2, str3;
-	scanf("%s %s", &str1, &str2);
-	str3 = str1[str1.size() - 1] - '0' + str2[str2.size() - 1] - '0';
-	str1.pop_back();
-	str2.pop_back();
-	a = str3[0] / 10;
-	str3[0] = (str3[0] % 10) + '0';
-	while (str1.size() > 0 || str2.size() > 0) {
-		b = 0;
-		if (str1.size()) {
-			b = str1[str1.size() - 1] - '0';
-			str1.pop_back();
-		}
-		if (str2.size()) {
-			b = b + str2[str2.size() - 1] - '0';
-			str2.pop_back();
-		}
-		b = b + a;
-		a = b / 10;
-		b = (b % 10) + '0';
-		str3 = b + str3;
-	}
-	if (a > 0) {
-		a = a + '0';
-		str3 = a + str3;
-	}
-	printf("%s", str3);
-	return 0;
-}
-*/
