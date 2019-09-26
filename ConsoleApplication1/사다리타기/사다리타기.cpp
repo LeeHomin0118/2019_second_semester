aa4aafdcebf64e3441e3b86983fd16a6ff38ca30
이호민 <leehomin118@gmail.com>
2019-09-26 20:59:38 +0900

    add ladder_game

--- ConsoleApplication1/사다리타기/사다리타기.cpp
+++ ConsoleApplication1/사다리타기/사다리타기.cpp
@@ -3,59 +3,131 @@
 #include <random>
 using namespace std;
 
-int arr[7][31];
+//변수 people의 값을 바꾸면 몇명을 추첨할지 정할 수 있습니다.
+const int people = 5;
+
+int arr[people + 2][(people * 10 + 11)];
+int result[people][people] = { 0 };
+int find_random[10][people][people] = { 0 };
 
 int create_random_number(int min, int max);
 void create_ladder();
-void print_ladder();
 void ladder(int k, int rand);
+void print_ladder();
+void print_result();
 
 int main() {
-	for (int i = 0; i < 10; i++) {
-		printf("\n*****%d*****\n", i);
-		create_ladder();
-	}
-}
-
-void create_ladder() {
-	for (int j = 0; j < 7; j++) {
-		for (int i = 0; i < 31; i++) {
-			arr[j][i] = 0;
+	for (int j = 0; j < 10; j++) {
+		for (int i = 0; i < people; i++)
+			for (int j = 0; j < people; j++)
+				result[i][j] = 0;
+		for (int i = 0; i < 100; i++) {
+			printf("\n%d 번째 사다리\n", i + 1);
+			create_ladder();
 		}
+		for (int i = 0; i < people; i++)
+			for (int k = 0; k < people; k++)
+				find_random[j][i][k] = result[i][k];
 	}
-	for (int i = 1; i <= 4; i++) {
-		ladder(i, create_random_number(4, 7));
-	}
-	print_ladder();
-}
-void ladder(int k, int rand) {
-	while (1) {
-		for (int i = 1; i < 30; i++) {
-			bool tmp = (arr[k][i] == 0 && arr[k][i - 1] == 0 && arr[k][i + 1] == 0);
-			if (create_random_number(0, 4) == 2 && rand!=0 && tmp) {
-				arr[k][i] = k;
-				arr[k + 1][i] = k;
-				rand--;
+	printf("랜덤의 편향성 검증\n");
+	for (int k = 0; k < 10; k++) {
+		printf("%d번째 사다리 게임\n", k + 1);
+		for (int i = 0; i < people; i++) {
+			printf("%c:( ", i + 65);
+			for (int j = 0; j < people; j++) {
+				printf("%d : %03d ", j + 1, find_random[k][i][j]);
 			}
+			printf(")\n");
 		}
-		if (rand == 0) break;
+		printf("\n");
 	}
-	//printf("\n***%d***\n", k);
-	//print_ladder();
-}
-void print_ladder() {
-	for (int i = 0; i < 31; i++) {
-		for (int j = 1; j <= 5; j++) {
-			if (arr[j][i] == j) {
-				printf("|-----");
+	for (int i = 0; i < people; i++) {
+		int sum[people] = { 0 };
+		for (int k = 0; k < 10; k++) {
+			printf("%02d번째%c:( ", k + 1, i + 65);
+			for (int j = 0; j < people; j++) {
+				printf("%d : %03d ", j + 1, find_random[k][i][j]);
+				sum[j] += find_random[k][i][j];
 			}
-			else {
-				printf("|     ");
-			}
+			printf(")\n");
+		}
+		printf("%c의 평균:\n", i + 65);
+		for (int j = 0; j < people; j++) {
+			printf("%d의 평균: %02d ", j + 1, sum[j] / 10);
 		}
 		printf("\n");
 	}
 }
+
+void create_ladder() {
+	for (int j = 0; j < people + 2; j++)
+		for (int i = 0; i < people * 11; i++)
+			arr[j][i] = 0;
+	for (int i = 1; i < people; i++)
+		ladder(i, create_random_number(people, people * 2));
+	print_ladder();
+	print_result();
+}
+
+void ladder(int k, int random_number) {
+	int cnt = 0;
+	while (1) {
+		for (int i = 1; i < (people * 10 + 10); i+=create_random_number(1, 3)) {
+			cnt++;
+			if (random_number == 0) break;
+			bool tmp = (arr[k][i] == 0 && arr[k][i - 1] == 0 && arr[k][i + 1] == 0);
+			if (create_random_number(0, 4) == 2 && random_number!=0 && tmp) {
+				arr[k][i] = k;
+				arr[k + 1][i] = k;
+				random_number--;
+			}
+		}
+		if (random_number == 0) break;
+	}
+	//printf("ladder : %d\n", cnt);
+	//printf("\n***%d***\n", k);
+	//print_ladder();
+}
+
+void print_ladder() {
+	for (int i = 0; i < people; i++)
+		printf("%c          ", i + 65);
+	printf("\n");
+	for (int i = 0; i < (people * 10 + 11); i++) {
+		for (int j = 1; j <= people; j++) {
+			if (arr[j][i] == j)
+				printf("|----------");
+			else
+				printf("|          ");
+		}
+		printf("\n");
+	}
+	for (int i = 0; i < people; i++)
+		printf("%d          ", i + 1);
+}
+
+void print_result() {
+	printf("\n이번 턴의 결과\n");
+	for (int i = 1; i <= people; i++) {
+		int tmp = i;
+		for (int j = 0; j < (people * 10 + 11); j++) {
+			if (arr[tmp][j] == tmp) tmp++;
+			else if (arr[tmp][j] == tmp - 1 && arr[tmp][j] != 0) tmp--;
+		}
+		printf("%c : %d     ", i + 64, tmp--);
+		result[i-1][tmp]++;
+	}
+	printf("\n지금까지의 총 결과\n");
+	for (int i = 0; i < people; i++) {
+		printf("%c:( ", i + 65);
+		for (int j = 0; j < people; j++) {
+			printf("%d : %03d ", j + 1, result[i][j]);
+		}
+		printf(")\n");
+	}
+	printf("\n");
+}
+
 int create_random_number(int min, int max) {
 	random_device rnd;
 	mt19937_64 rng(rnd());
