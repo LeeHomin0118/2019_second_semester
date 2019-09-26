375ffb21e0d968647a08683e5ed66e96a6768901
LeeHomin0118 <leehomin118@gmail.com>
2019-09-26 18:47:41 +0900

    add ladder_game

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,16 @@
-﻿#include <stdio.h>
-#include <vector>
-#include <algorithm>
-using namespace std;
-typedef pair<int, int> arr;
-int arr[1000000];
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 int main() {
-	int n, m, c1, c2;
-	scanf("%d %d %d %d", &n, &m, &c1, &c2);
-	for (int i = 0; i < n; i++) {
-		int tmp;
-		scanf("%d", &tmp);
-		arr[i] = { tmp, 0 };
+	int number[3][4] = { {1, 3, 5, 7}, {9, 11, 13, 15}, {17, 19, 21, 23} };
+	int i, j, *p;
+	p = &number[0][0];
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 4; j++) {
+			printf("%d\n", *(*(number+i)+j));
+		}
 	}
-	for (int i = 0; i < m; i++) {
-		int tmp;
-		scanf("%d", &tmp);
-		arr[n + i] = { tmp, 1 };
-	}
+	/*
+	1. *(*(number+i)+j)*(p+i*1+j)
+	2. *(p+i*1+j)
+	*/
 }
---
+++ ConsoleApplication1/사다리타기/사다리타기.cpp
@@ -0,0 +1,65 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <random>
+using namespace std;
+
+int arr[7][31];
+
+int create_random_number(int min, int max);
+void create_ladder();
+void print_ladder();
+void ladder(int k, int rand);
+
+int main() {
+	for (int i = 0; i < 10; i++) {
+		printf("\n*****%d*****\n", i);
+		create_ladder();
+	}
+}
+
+void create_ladder() {
+	for (int j = 0; j < 7; j++) {
+		for (int i = 0; i < 31; i++) {
+			arr[j][i] = 0;
+		}
+	}
+	for (int i = 1; i <= 4; i++) {
+		ladder(i, create_random_number(4, 7));
+	}
+	print_ladder();
+}
+void ladder(int k, int rand) {
+	while (1) {
+		for (int i = 1; i < 30; i++) {
+			bool tmp = (arr[k][i] == 0 && arr[k][i - 1] == 0 && arr[k][i + 1] == 0);
+			if (create_random_number(0, 4) == 2 && rand!=0 && tmp) {
+				arr[k][i] = k;
+				arr[k + 1][i] = k;
+				rand--;
+			}
+		}
+		if (rand == 0) break;
+	}
+	//printf("\n***%d***\n", k);
+	//print_ladder();
+}
+void print_ladder() {
+	for (int i = 0; i < 31; i++) {
+		for (int j = 1; j <= 5; j++) {
+			if (arr[j][i] == j) {
+				printf("|-----");
+			}
+			else {
+				printf("|     ");
+			}
+		}
+		printf("\n");
+	}
+}
+int create_random_number(int min, int max) {
+	random_device rnd;
+	mt19937_64 rng(rnd());
+	uniform_int_distribution<int> val(min, max);
+	int n = val(rng);
+	return n;
+}
