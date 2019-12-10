cb893880455eda3208598741c70e571823153bb8
LeeHomin0118 <leehomin118@gmail.com>
2019-12-11 01:29:26 +0900

    아니 개 씨발 과제를 씨발 진짜 씨발 진짜 씨발 진짜댈퓨ㅗㄷㄱ호ㅑㄴㄱ유포ㅓㄴㅇㄹ포ㅠㄴㄹ포 ㄴ오ㅠㄷ공런퍼ㅗㅈㄹ퓨ㅘㅣㅇ류 핀ㅇㄹ퓨ㅑㅚㄴ유퍈ㅇㄹ 퐞듀피ㅑㅘㄴㅇ류파ㅗ포ㅑㅣㄷ가ㅠ폰ㄷ가ㅠ퍄ㅕㅈ가뉴ㅗㄱ섶

--- ConsoleApplication1/압축파일목록보기/압축파일목록보기.c
+++ ConsoleApplication1/압축파일목록보기/압축파일목록보기.c
@@ -36,23 +36,10 @@ typedef struct CENTRAL_FILE_HEADER {
 	ui ea;
 	ui ro;
 } central_header;
-typedef struct LOCAL_FILE_HEADER {
-	ui signature;
-	us ver;
-	us bitflag;
-	us comp_method;
-	us last_mod_time;
-	us last_mod_date;
-	ui crc_32;
-	ui comp_size;
-	ui uncomp_size;
-	us name_len;
-	us ex_len;
-} local_header;
 #pragma pack(pop)
 
 int main(int argc, char* argv[]) {
-	FILE *fp1 = fopen("file.zip", "r");
+	FILE *fp1 = fopen(argv[1], "r");
 	if (fp1 == NULL) {
 		printf("fale to open file.");
 		return 0;
@@ -71,6 +58,7 @@ int main(int argc, char* argv[]) {
 		}
 	}
 	fseek(fp1, endh.offset, SEEK_SET);
+	printf("file.zip 안에 포함된 파일은 다음과 같습니다.\n");
 	while (!feof(fp1)) {
 		central_header tmp;
 		fread(&tmp, 1, sizeof(tmp), fp1);
@@ -79,7 +67,7 @@ int main(int argc, char* argv[]) {
 				printf("-모두 %d개의 파일(%d B) 입니다.\n", cnt - 1, size_of_all_file);
 			}
 			else {
-				printf("-모두 %d개의 파일(%d KB) 입니다.\n", cnt - 1, size_of_all_file/1000);
+				printf("-모두 %d개의 파일(%d KB) 입니다.\n", cnt - 1, size_of_all_file / 1000);
 			}
 			break;
 		}
@@ -94,32 +82,12 @@ int main(int argc, char* argv[]) {
 			printf("%d. %s %d Bytes\n", cnt++, file_name, tmp.uncompsize);
 		}
 		else {
-			printf("%d. %s %d Kbytes\n", cnt++, file_name, tmp.uncompsize/1000);
+			printf("%d. %s %d Kbytes\n", cnt++, file_name, tmp.uncompsize / 1000);
 		}
 		size_of_all_file += tmp.uncompsize;
 		free(file_name);
 		free(ex_data);
 		free(comment);
 	}
-	/*
-	이 코드는 로컬 헤더들 사이에 있는 데이터들의 값을 고려하지 못했음
-	while (1) {
-		local_header tmp;
-		fread(&tmp, 1, sizeof(tmp), fp1);
-		if (tmp.signature != 0x04034b50) {
-			printf("-모두 %d개의 파일(%d) 입니다.\n", cnt - 1, size_of_all_file);
-			break;
-		}
-		//파일 이름과 추가항목의 길이가 있는데 읽지 않으면 다음 시그니처를 읽을 때 값이 달라 오류가 날 수 있음.
-		char *file_name = (char*)malloc(tmp.name_len + 1);
-		char *ex_data = (char*)malloc(tmp.ex_len);
-		char *data = (char*)malloc(tmp.comp_size);
-		fread(file_name, 1, tmp.name_len, fp1);
-		fread(ex_data, 1, tmp.ex_len, fp1);
-		file_name[tmp.name_len] = NULL;
-		printf("%d. %s %d\n", cnt++, file_name, tmp.uncomp_size);
-		size_of_all_file += tmp.uncomp_size;
-	}
-	*/
 	fclose(fp1);
 }
